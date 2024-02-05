3a2338424d6a5c9911032585be784be9f3749ca2
iRichard <zerorichardx@gmail.com>
2024-02-05 04:01:20 -0500

    Fizz buzz Practice

---
+++ day_seven/fizzz_buzz.py
@@ -0,0 +1,11 @@
+target = int(input("Please introduce a number: "))
+
+for num in range(0,target+1):
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
